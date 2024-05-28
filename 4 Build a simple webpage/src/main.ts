2d9d47563716a0dcfc1400a5b785155f9f4b151e
Yuwei Teh <ywteh13@gmail.com>
2024-05-28 15:44:09 +0800

    Fix stuff

--- 4 Build a simple webpage/src/main.ts
+++ 4 Build a simple webpage/src/main.ts
@@ -5,13 +5,9 @@ import "./style.css";
 const imageGrid = document.getElementById('image-grid')!;
 const sortDropdown = document.getElementById('sort-dropdown')! as HTMLSelectElement;
 
-// TODO default sort order 
-
 const insertCards = (sortValue: string = "ASC") => {
   // remove all card item from the DOM if there's any 
-  while (imageGrid.firstChild) {
-    imageGrid.removeChild(imageGrid.firstChild);
-  }
+  imageGrid.innerHTML = "";
   sortCards(sortValue);
 
   cardsData.forEach(data => {
@@ -19,7 +15,7 @@ const insertCards = (sortValue: string = "ASC") => {
     card.className = 'card';
     card.innerHTML = `
       <div class="img-wrapper">
-        <img class="img" src="https://data.cardsandhobbies.com/images/1778cf9a-2703-41e9-86d7-ec21af8cf61d/card.jpg" alt="">
+        <img class="img" src="${data.imageSrc}" alt="">
       </div>
       <div class="info">
         <div class="sm-text">
@@ -35,11 +31,7 @@ const insertCards = (sortValue: string = "ASC") => {
 };
 
 const sortCards = (sortValue: string) => {
-  if (sortValue === "ASC") {
-    cardsData.sort((a, b) => a.collectorNo - b.collectorNo);  
-  } else if (sortValue === "DESC") {
-    cardsData.sort((a, b) => b.collectorNo - a.collectorNo);  
-  }
+  cardsData.sort((a, b) => sortValue === "ASC" ? a.collectorNo - b.collectorNo : b.collectorNo - a.collectorNo);  
 }
 
 insertCards();
